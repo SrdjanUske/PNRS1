033392ba19ab66b56c9435a5d6972ccc5d88eed2
SrdjanUske <susorac96@gmail.com>
2018-03-31 01:17:10 +0200

    verzija Chat-a, koja radi

---
+++ ChatApplication/app/src/main/java/srdjan/usorac/chatapplication/CharacterAdapter.java
@@ -0,0 +1,85 @@
+package srdjan.usorac.chatapplication;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+public class CharacterAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private ArrayList<Custom> mCharacters;
+
+    public CharacterAdapter(Context mContext) {
+        this.mContext = mContext;
+        mCharacters = new ArrayList<Custom>();
+    }
+
+    public void addCharacter(Custom character) {
+        mCharacters.add(character);
+        notifyDataSetChanged();
+    }
+
+
+    @Override
+    public int getCount() {
+        return mCharacters.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        Object rv = null;
+
+        try {
+            rv = mCharacters.get(i);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return rv;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(
+                    Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.character_row, null);
+            ViewHolder holder = new ViewHolder();
+            holder.image = (ImageView) view.findViewById(R.id.imageView);
+            holder.name = (TextView) view.findViewById(R.id.contact_name);
+            holder.first_letter = (TextView) view.findViewById(R.id.first_letter);
+
+            holder.first_letter.setBackgroundColor(Color.rgb((int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255)));
+            view.setTag(holder);
+        }
+
+        Custom character = (Custom) getItem(i);
+        ViewHolder holder = (ViewHolder) view.getTag();
+        holder.image.setImageDrawable(character.getImage());
+        holder.name.setText(String.valueOf(character.getName()));
+        holder.first_letter.setText(String.valueOf(character.getFirst_letter()));
+
+
+        return view;
+    }
+
+    private class ViewHolder {
+        public TextView first_letter = null;
+        public TextView name = null;
+        public ImageView image = null;
+    }
+
+}
--- ChatApplication/app/src/main/java/srdjan/usorac/chatapplication/ContactsActivity.java
+++ ChatApplication/app/src/main/java/srdjan/usorac/chatapplication/ContactsActivity.java
@@ -5,22 +5,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class ContactsActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button logout;
-    TextView my_friend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+
         logout = findViewById(R.id.logout);
         logout.setOnClickListener(this);
 
-        my_friend = (TextView) findViewById(R.id.my_friend);
-        my_friend.setOnClickListener(this);
+        CharacterAdapter adapter = new CharacterAdapter(this);
+        adapter.addCharacter(new Custom((Character)getString(R.string.savo_dragovic).charAt(0),
+                                        getString(R.string.savo_dragovic),
+                                        getResources().getDrawable(R.drawable.eric_cartman)));
+        adapter.addCharacter(new Custom((Character)getString(R.string.marko_markovic).charAt(0),
+                                        getString(R.string.marko_markovic),
+                                        getResources().getDrawable(R.drawable.eric_cartman)));
+        adapter.addCharacter(new Custom((Character)getString(R.string.srdjan_usorac).charAt(0),
+                                        getString(R.string.srdjan_usorac),
+                                        getResources().getDrawable(R.drawable.eric_cartman)));
+
+        ListView list = (ListView) findViewById(R.id.contacts_list);
+        list.setAdapter(adapter);
 
     }
 
@@ -31,10 +43,6 @@ public class ContactsActivity extends AppCompatActivity implements View.OnClickL
 
         if (view.getId() == R.id.logout) {
             intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-        }
-        else if (view.getId() == R.id.my_friend) {
-            intent = new Intent(this, MessageActivity.class);
             startActivity(intent);
         }
     }
