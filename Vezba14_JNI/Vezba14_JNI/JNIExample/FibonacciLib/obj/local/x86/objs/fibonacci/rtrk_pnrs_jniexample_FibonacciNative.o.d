dd02dbd430b1568d8ea2bbff9d03052275acf4f8
Srki <susorac96@gmail.com>
2018-06-02 14:54:05 +0200

    --Vezba14_JNI

--- Vezba14_JNI/Vezba14_JNI/JNIExample/FibonacciLib/obj/local/x86/objs/fibonacci/rtrk_pnrs_jniexample_FibonacciNative.o.d
+++
@@ -1,5 +0,0 @@
-./obj/local/x86/objs/fibonacci/rtrk_pnrs_jniexample_FibonacciNative.o: \
-  jni/rtrk_pnrs_jniexample_FibonacciNative.c \
-  jni/rtrk_pnrs_jniexample_FibonacciNative.h
-
-jni/rtrk_pnrs_jniexample_FibonacciNative.h:
